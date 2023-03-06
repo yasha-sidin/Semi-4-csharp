c14c4ad80834f2dbf3933fdd3adb81a5c4387a2d
Yasha <yashasidin333@yandex.ru>
2023-03-06 20:37:13 +0300

    Add ex_1

---
+++ ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Given an integer x, return true if is a palindrom, and false otherwise
+bool IsPalindrom(int number)
+{
+    int revNumber = 0;
+    int sourceNumber = number;
+    while (sourceNumber != 0)
+    {
+        int pop = sourceNumber % 10;
+        sourceNumber = sourceNumber / 10;
+
+        revNumber = (revNumber * 10) + pop;
+    }
+    bool isResult = revNumber == number;
+
+    return isResult;
+}
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(IsPalindrom(number));
+
